e4e1849c938048c29021a623dddc84ebf4c6023b
azakimi123 <aaron.zakimi@gmail.com>
2020-08-05 14:23:05 -0600

    chart of popular items added(testing)

--- src/Components/Chart2/Chart2.js
+++ src/Components/Chart2/Chart2.js
@@ -1,10 +1,50 @@
 import React, {Component} from 'react';
+import { Bar, Line, Pie } from 'react-chartjs-2';
+import '../../App.scss'
 
 class Chart2 extends Component {
-    render(){
-        return (
-            <div>
-                <p>Chart2</p>
+    constructor(props) {
+        super(props);
+        this.state = {
+            chartData: {
+                labels: ['Peony', 'Rose', 'Wild Flower', 'Carnation', 'Cake Topper', 'Birthday'],
+                datasets: [
+                    {
+                        label: 'Sales',
+                        data: [4, 5, 1, 8, 2, 2],
+                        backgroundColor: ['rgba(255, 99, 132, 0.5)', 'rgb(207, 252, 255, 0.5)', 'rgb(170, 239, 223, 0.5)', 'rgba(158, 227, 125, 0.5)', 'rgba(99, 193, 50, 0.5)', 'rgba(53, 134, 0, 0.5)',]
+                    }
+                ]
+            }
+        }
+    }
+
+    static defaultProps = {
+        displayTitle: true,
+        displayLegend: true,
+        legendPosition: 'right'
+    }
+    render() {
+        return(
+            <div className='chart-container'>
+                <div className='chart'>
+                    <Bar
+                        data={this.state.chartData}
+                        width={100}
+                        height={50}
+                        options={{
+                            title: {
+                                display: true,
+                                text: 'Lynne and Lee Sales',
+                                fontSize: 25
+                            },
+                            legend: {
+                                display: true,
+                                position: 'right',
+                            }
+                        }}
+                    />
+                </div>
             </div>
         )
     }
--- src/Components/ShopStats/ShopStats.js
+++ src/Components/ShopStats/ShopStats.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Chart2 from '../Chart2/Chart2';
 
 function ShopStats() {
     return (
         <div>
             <p>ShopStats</p>
+            <Chart2 />
         </div>
     )
 }
